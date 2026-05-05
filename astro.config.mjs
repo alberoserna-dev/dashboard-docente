ba4144c42aa291935f6ff442b16dd62c8c839696
José Antonio Albero <alberoserna.dev@gmail.com>
2026-05-05 11:08:41 +0200

    Url config para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import { remarkCallout } from '@r4ai/remark-callout';
 
 export default defineConfig({
+  site: 'https://alberoserna-dev.github.io',
+  base: '/dashboard-docente',
   markdown: {
     remarkPlugins: [remarkCallout],
   },
